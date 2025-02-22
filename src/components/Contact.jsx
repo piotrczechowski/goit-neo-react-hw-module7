ed0fa7c1f8cb656ac6557d28ccd1b3214659378b
piotrczechowski <58622514+piotrczechowski@users.noreply.github.com>
2025-02-22 17:34:51 +0100

    add stylong to contact list

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,29 @@
-import styles from "./Contact.module.css";
+import React from "react";
 import PropTypes from "prop-types";
+import styles from "./Contact.module.css";
+// Import desired icons from react-icons
+import { FaUser, FaPhone, FaTrashAlt } from "react-icons/fa";
 
 function Contact({ contact, onDeleteContact }) {
   return (
     <div className={styles.contactItem}>
-      <span>
-        {contact.name} : {contact.number}
-      </span>
+      {/* Contact info (name + phone number) */}
+      <div className={styles.contactInfo}>
+        <div className={styles.contactName}>
+          <FaUser className={styles.icon} />
+          {contact.name}
+        </div>
+        <div className={styles.contactNumber}>
+          <FaPhone className={styles.icon} />
+          {contact.number}
+        </div>
+      </div>
+
       <button
         className={styles.contactDeleteButton}
         onClick={() => onDeleteContact(contact.id)}
       >
-        Delete
+        <FaTrashAlt /> Delete
       </button>
     </div>
   );
