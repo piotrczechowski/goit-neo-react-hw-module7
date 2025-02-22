ec483d1895f0c88b883b967af9fc3e2b4fdd74e6
piotrczechowski <58622514+piotrczechowski@users.noreply.github.com>
2025-02-22 15:12:22 +0100

    add search box

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,5 +1,28 @@
+import PropTypes from "prop-types";
 import styles from "./SearchBox.module.css";
 
 function SearchBox({ filter, onChange }) {
-  return <div className={styles.searchBox.Container}></div>;
+  return (
+    <div className={styles.searchBoxContainer}>
+      <label htmlFor="search" className={styles.searchBoxLabel}>
+        Find contact by name:
+      </label>
+
+      <input
+        id="search"
+        type="text"
+        value={filter}
+        onChange={onChange}
+        placeholder="Search by name"
+        className={styles.searchBoxInput}
+      />
+    </div>
+  );
 }
+
+export default SearchBox;
+
+SearchBox.propTypes = {
+  filter: PropTypes.string.isRequired,
+  onChange: PropTypes.string.isRequired,
+};
